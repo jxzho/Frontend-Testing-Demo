b317bd9a3d2842d2656cd0095d752f4a22e7e089
junxio <me@junxio.site>
2020-04-06 23:46:44 +0800

    feat: Hooks

--- LS7/Counter.test.js
+++ LS7/Counter.test.js
@@ -2,24 +2,33 @@ import Counter from './Counter'
 
 let counter = undefined
 
-beforeAll(() => {
-  console.log('beforeAll');
-})
+// beforeAll(() => {
+//   console.log('beforeAll');
+// })
 
-beforeEach(() => {
-  counter = new Counter()
-})
+// beforeEach(() => {
+//   counter = new Counter()
+// })
 
-afterEach(() => {
-  console.log('afterEach');
-})
+// afterEach(() => {
+//   console.log('afterEach');
+// })
 
-afterAll(() => {
-  console.log('afterAll');
-})
+// afterAll(() => {
+//   console.log('afterAll');
+// })
 
 describe('测试类 Counter', () => {
+  beforeEach(() => {
+    counter = new Counter
+    console.log('beforeEach');
+  })
+
   describe('测试加法相关代码', () => {
+    beforeEach(() => {
+      console.log('beforeEach');
+    })
+
     test('测试加法 addOne', () => {
       counter.addOne()
       expect(counter.number).toBe(1)
